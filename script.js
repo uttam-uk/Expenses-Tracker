042d3f8fa58da5730f82764f8dd379c488e34222
Uttam Anand <103377366+uttam-uk@users.noreply.github.com>
2022-11-01 12:40:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -54,27 +54,45 @@ function addTransactionDOM(transaction) {
   // Add class based on value
   item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
 
-  item.innerHTML = `₹{transaction.text} <span>₹{sign}₹{Math.abs(transaction.amount)}</span> <button class="delete-btn" onclick="removeTransaction(₹{transaction.id})">x</button>`;
+  item.innerHTML = `
+    ₹{transaction.text} <span>₹{sign}₹{Math.abs(
+    transaction.amount
+  )}</span> <button class="delete-btn" onclick="removeTransaction(₹{
+    transaction.id
+  })">x</button>
+  `;
+  
   list.appendChild(item);
 }
 
 // Update the balance, income and expense
 function updateValues() {
   const amounts = transactions.map(transaction => transaction.amount);
+
   const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
 
-  const income = amounts.filter(item => item > 0).reduce((acc, item) => (acc += item), 0).toFixed(2);
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
   const expense = (
-  amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) * -1).toFixed(2);
+    amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+
   balance.innerText = `₹₹{total}`;
   money_plus.innerText = `₹₹{income}`;
   money_minus.innerText = `₹₹{expense}`;
 }
 
+
 // Remove transaction by ID
 function removeTransaction(id) {
   transactions = transactions.filter(transaction => transaction.id !== id);
+  
   updateLocalStorage();
+  
   init();
 }
 
@@ -85,9 +103,12 @@ function updateLocalStorage() {
 
 // Init app
 function init() {
-  list.innerHTML = ` `;
+  list.innerHTML = '';
+
   transactions.forEach(addTransactionDOM);
   updateValues();
 }
+
 init();
+
 form.addEventListener('submit', addTransaction);
